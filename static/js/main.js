8bdcc683f4667117af3596d16e34590f9478eec9
Ron Snyder <ron@snyderjr.com>
2022-05-31 22:20:24 -0400

    styling

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,5 @@
 window.isMobile = ('ontouchstart' in document.documentElement && /mobi/i.test(navigator.userAgent) )
+window.isEditor = location.port === '5555' || location.hostname.indexOf('editor') == 0
 console.log(`main.js: isMobile=${window.isMobile} height=${window.innerHeight}`)
 
 // Use ScrollMagic to set paragraphs as 'active'
@@ -7,8 +8,8 @@ window.controller = new ScrollMagic.Controller({globalSceneOptions: {}})
 Array.from(document.querySelectorAll('p')).forEach(p => {  // build scenes
     new ScrollMagic.Scene({
             triggerElement: p,
-            triggerHook: window.isMobile ? 0.5 : 200/window.innerHeight, 
-            offset: -20,
+            triggerHook: window.isMobile ? 0.6 : (window.isEditor ? 300 : 200)/window.innerHeight, 
+            offset: -40,
             duration: p.clientHeight
         })
         .setClassToggle(p, 'active') // add class toggle
